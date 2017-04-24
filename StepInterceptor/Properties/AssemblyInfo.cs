0432a4b201a3272f3a5ff4676872354620b78dd2
rob.fahey <rob.fahey@asos.com>
2017-04-24 15:42:56 +0100

    updated nuspec description

--- StepInterceptor/Properties/AssemblyInfo.cs
+++ StepInterceptor/Properties/AssemblyInfo.cs
@@ -8,9 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("StepInteceptor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ASOS.com")]
 [assembly: AssemblyProduct("StepInteceptor")]
-[assembly: AssemblyCopyright("Copyright © ASOS.com 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
